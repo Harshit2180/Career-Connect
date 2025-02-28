389c0c050b3e9e6362af5b2db22589f76158aaeb
Harshit2180 <chopraharshit2180@gmail.com>
2025-02-28 18:53:59 +0530

    Cloudinary setup with updateCompany

--- Backend/controllers/company.controller.js
+++ Backend/controllers/company.controller.js
@@ -1,15 +1,17 @@
-import {Company} from "../models/company.models.js"
+import { Company } from "../models/company.models.js"
+import getDataUri from "../utils/datauri.js";
+import cloudinary from "../utils/cloudinary.js";
 
 export const registerCompany = async (req, res) => {
     try {
-        const {companyName} = req.body;
+        const { companyName } = req.body;
         if (!companyName) {
             return res.status(400).json({
                 message: "Company name is required",
                 success: false
             })
         }
-        let company = await Company.findOne({name: companyName});
+        let company = await Company.findOne({ name: companyName });
         if (company) { // here
             return res.status(400).json({
                 message: "You can't register same company",
@@ -33,7 +35,7 @@ export const registerCompany = async (req, res) => {
 export const getCompany = async (req, res) => {
     try {
         const userId = req.id;
-        const companies = await Company.find({userId});
+        const companies = await Company.find({ userId });
         if (!companies) {
             return res.status(404).json({
                 message: "Companies not found",
@@ -53,7 +55,7 @@ export const getCompanyById = async (req, res) => {
     try {
         const companyId = req.params.id;
         const company = await Company.findById(companyId);
-        if(!company) {
+        if (!company) {
             return res.status(404).json({
                 message: "Company not found",
                 success: false
@@ -70,12 +72,16 @@ export const getCompanyById = async (req, res) => {
 
 export const updateCompany = async (req, res) => {
     try {
-        const { name, description, website, location} = req.body;
+        const { name, description, website, location } = req.body;
         const file = req.file;
 
-        const updateData = {name, description, website, location};
+        const fileUri = getDataUri(file);
+        const cloudResponse = await cloudinary.uploader.upload(fileUri.content);
+        const logo = cloudResponse.secure_url;
 
-        const company = await Company.findByIdAndUpdate(req.params.id, updateData, {new: true});
+        const updateData = { name, description, website, location, logo };
+
+        const company = await Company.findByIdAndUpdate(req.params.id, updateData, { new: true });
         if (!company) {
             return res.status(404).json({
                 message: "Company not found",
